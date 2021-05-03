0ded6118ecdd3efff5c3103bf24495512cc49ed3
as-Solo <asdelsolo@gmail.com>
2021-05-03 11:28:24 +0200

    03/05/2021 11:28

--- main.py
+++ main.py
@@ -38,6 +38,41 @@ def index():
     ms_template = markdown.markdown(readme_file.read(), extensions = ['fenced_code'])
     return ms_template
 
+@app.route('/<name>')
+def autor(name):
+    frases = get.mensaje_autor_HTML(name)
+    return frases
+
+@app.route('/random')
+def aleatorio():
+    frase = get.mensaje_aleatorio_HTML()
+    return jsonify(frase)
+
+@app.route('/mensaje_autor<nombre>')
+def mensaje_autor(nombre):
+    frase = get.mensaje_autor(nombre)
+    return jsonify(frase)
+
+@app.route('/citas_positivas<mayor>_<numero>')
+def citas_pos(mayor, numero):
+    citas = get.citas_positivas(mayor, numero)
+    return jsonify(citas)
+
+@app.route('/citas_negativas<mayor>_<numero>')
+def citas_neg(mayor, numero):
+    citas = get.citas_negativas(mayor, numero)
+    return jsonify(citas)
+
+@app.route('/citas_compound_pos<mayor>_<numero>')
+def citas_com_pos(mayor, numero):
+    citas = get.citas_compound_pos(mayor, numero)
+    return jsonify(citas)
+
+@app.route('/citas_compound_neg<menor>_<numero>')
+def citas_com_neg(menor, numero):
+    citas = get.citas_compound_neg(menor, numero)
+    return jsonify(citas)
+
 
 #----------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------
--- tools/get_data.py
+++ tools/get_data.py
@@ -5,21 +5,68 @@
 #----------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------
 
+import random
 from config.configuration import db, coleccion
 
 #----------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------
 
 
-def mensaje_autor(personaje):
-    query = {"author" : f'{personaje}'}
-    frases = list(collection.find(query, {"_id" : 0}))
-    return frases
+def mensaje_autor_HTML(personaje):
+    #implementado
+    personaje = personaje.title()
+    query = {"autor" : f'{personaje}'}
+    frases = list(coleccion.find(query, {"_id" : 0, 'cita' : 1}))
+    cadena = str()
+    numero = 1
+    for elem in frases:
+        cadena += str(numero) + '.- "' + str(elem["cita"]) + '"' + "<br />" + "<br />"
+        numero += 1
+    if len(cadena) > 0:
+        return cadena
+    else:
+        return 'No se han encontrado coincidencias'
 
-def mensaje_personaje():
-    query = {"character_name" : 'Albus Dumbledore'}
-    frases = list(collection.find(query, {"_id" : 0}))
+def mensaje_autor(personaje):
+    #implementado
+    personaje = personaje.title()
+    query = {"autor" : f'{personaje}'}
+    frases = list(coleccion.find(query, {"_id" : 0, 'cita' : 1}))
     return frases
+    
+def mensaje_aleatorio_HTML():
+    #implementado
+    numero = random.randint(0, 2964)
+    query = {"_id" : numero}
+    frase_aleatoria = list(coleccion.find(query, {"_id" : 0, 'cita' : 1}))
+    return frase_aleatoria[0]['cita']
+
+def mensaje_aleatorio():
+    #implementado
+    numero = random.randint(0, 2964)
+    query = {"_id" : numero}
+    frase_aleatoria = list(coleccion.find(query, {"_id" : 0, 'cita' : 1}))
+    return frase_aleatoria
+
+
+def citas_positivas(mayor_que, numero_citas):
+    #implementado
+    return list(coleccion.find({'cita_sentiment_(P/N/C).0' : {'$gte' : float(mayor_que)}}, {"cita_sentiment_(P/N/C)" : {'$slice' : [0,1]}, 'cita' : 1, 'autor' : 1, '_id' : 0}).limit(int(numero_citas)) )
+
+
+def citas_negativas(mayor_que, numero_citas):
+    return list(coleccion.find({'cita_sentiment_(P/N/C).1' : {'$gte' : float(mayor_que)}}, {"cita_sentiment_(P/N/C)" : {'$slice' : [1,1]}, 'cita' : 1, 'autor' : 1, '_id' : 0}).limit(int(numero_citas)) )
+
+
+def citas_compound_pos(mayor_que, numero_citas):
+    return list(coleccion.find({'cita_sentiment_(P/N/C).2' : {'$gte' : float(mayor_que)}}, {"cita_sentiment_(P/N/C)" : {'$slice' : [2,3]}, 'cita' : 1, 'autor' : 1, '_id' : 0}).limit(int(numero_citas)) )
+
+
+def citas_compound_neg(menor_que, numero_citas):
+    return list(coleccion.find({'cita_sentiment_(P/N/C).2' : {'$lte' : float(menor_que)}}, {"cita_sentiment_(P/N/C)" : {'$slice' : [2,3]}, 'cita' : 1, 'autor' : 1, '_id' : 0}).limit(int(numero_citas)) )
+
+
+
 
 
 #-----------------------------------------------------------------------------------------
